2dd6fef09a9b2b791a88130e921b28f244f9d847
eriks junior <eriksjunior13@gmail.com>
2022-10-19 01:52:39 -0300

    feat: iniciado migração para styled components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,58 +1,51 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-} from "react-native";
+
+import { Container, Content, TextHeader, TouchableOpacityIcon } from "./styles";
 import { Feather } from "@expo/vector-icons";
 interface IUserName {
   headerText?: string;
   icon?: 'user'
 }
 
-const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
-
 export default function Header({ headerText, icon }: IUserName) {
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.textHeaderUserName}>{headerText}</Text>
+    <Container>
+      <Content>
+        <TextHeader>{headerText}</TextHeader>
 
-        <TouchableOpacity activeOpacity={0.5} style={styles.icon}>
+        <TouchableOpacityIcon activeOpacity={0.5}>
           <Feather name={icon} size={27} color="#fff" />
-        </TouchableOpacity>
-      </View>
-    </View>
+        </TouchableOpacityIcon>
+      </Content>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#181818",
-    paddingTop: statusBarHeight,
-    paddingStart: 16,
-    paddingEnd: 16,
-    paddingBottom: 50,
-    flexDirection: "row",
-  },
-  content: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  textHeaderUserName: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#009999",
-  },
-  icon: {
-    width: 44,
-    height: 44,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 44 / 2,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "#181818",
+//     paddingTop: statusBarHeight,
+//     paddingStart: 16,
+//     paddingEnd: 16,
+//     paddingBottom: 50,
+//     flexDirection: "row",
+//   },
+//   content: {
+//     flex: 1,
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "space-between",
+//   },
+//   textHeader: {
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     color: "#009999",
+//   },
+//   icon: {
+//     width: 44,
+//     height: 44,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     borderRadius: 44 / 2,
+//   },
+// });
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,13 +3,34 @@ import {
   StatusBar,
 } from "react-native";
 
-const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + "22" : "64";
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export const Container = styled.View`
-    background-color: "#181818";
-    padding-top: ${statusBarHeight};
-    padding-start: "16";
-  padding-end: "16";
-    padding-bottom: "50";
-    flex-direction: "row";
+  background-color: #181818;
+  padding-top: ${statusBarHeight}px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 50px;
+  flex-direction: row;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TextHeader = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #009999;
+`;
+
+export const TouchableOpacityIcon = styled.TouchableOpacity`
+  width: 44px;
+  height: 44px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
 `;
